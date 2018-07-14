f623b51a4c3b3adee219cac7a86885ad224876b3
Nathan Nguyen <natn2323@gmail.com>
2018-07-14 16:04:18 -0700

    Separating handlers based on 'entities' xD

--- public/javascript/dispatcher.js
+++ public/javascript/dispatcher.js
@@ -1,32 +1,51 @@
 'use strict';
 
-/* Code within the module.exports property is public and can be Accessed from
+/* Code within the module.exports property is public and can be accessed from
 	 other modules */
 module.exports = {
+  serve: function(request, response) {
+    var qs = require('querystring');
+    var requestBody = '';
 
-	deal: function(request, response) {
-		// Dealing with the GET requests
-		if(request.method === "GET") {
-				// Pass data to the appropriate function
-		    var get = require('./get');
-		    get.do(request, response);
-		} else if(request.method === "POST") { // Dealing with the POST requests
-			var qs = require('querystring');
-			var requestBody = '';
+    request.on('data', function(data) {
+        requestBody += data;
+    }); // on data
 
-			request.on('data', function(data) {
-				requestBody += data;
-			});
+    request.on('end', function() {
+      var qs = require('querystring');
+      var passed_data = qs.parse(requestBody);
 
-			request.on('end', function() {
-				var formData = qs.parse(requestBody);
-				var post = require('./post');
+      var path = require('path');
+      var parsed_path = path.parse(request.url);
+      var filename = parsed_path['name']+'.js';
+      var filepath = path.join(__dirname, filename);
 
-				// Pass data to the appropriate function
-				post.doing(request, response, formData);
-			});
+      var fs = require('fs');
+      //console.log("Referer is undefined?: "+request.headers['referer']);
+      /* Preventing direct access to /menu, etc. without first starting at
+         login screen by checking the 'referer', i.e. the previous page.
+         Might be an issue later when we have more pages linked together. */
+      if(request.url === "/") {
+        fs.readFile('./public/html/login.html', function(err, data) {
+        if(err) {
+          throw err;
+        } else {
+          response.writeHead(200, {'Content-Type': 'text/html'});
+          response.write(data);
+          response.end();
+        }
+        });
 
-		}
-	} // End deal
+      } else if(fs.existsSync(filepath)) { // This is blocking, synchronous code
+         // Associating URL with JavaScript file
+        var serving = require(filepath);
+        serving.handle(request, response, passed_data);
 
-} // End exports 
+      } else {
+        // Could also have an error.js that deals with various errors
+        console.log("Non-existent: "+filepath);
+
+      }
+    }) // on end
+  } // end serve
+} // end of file
---
+++ public/javascript/login.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  handle: function(request, response, data) {
+    console.log("Login.js entered!");
+    if(request.method === "GET") GETHandler(request, response);
+    else if(request.method === "POST") POSTHandler(request, response, data);
+  }
+}
+
+function GETHandler(request, response) {
+  var fs = require('fs');
+    fs.readFile('./public/html/login.html', function(err, data) {
+    if(err) {
+      throw err;
+    } else {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      response.end();
+    }
+  });
+} // end function
+
+function POSTHandler(request, response, data) {
+  var db_man = require("./DBManager.js");
+  var validationPromise = db_man.validate(data);
+  validationPromise.then(function(validated) {
+    if(validated) {
+      console.log("Validated!");
+      response.writeHead(301,
+        {Location: 'http://localhost:8124/menu'}
+      );
+      /* From documentation, after a "finish" event, no more events will be
+        emitted on the response object */
+      response.end();
+
+    } else {
+      /* Not validated, maybe do some frontend response or send to
+         loginError.html or the like */
+      response.writeHead(301,
+        {Location: 'http://localhost:8124/login'}
+      );
+      response.end();
+
+    }
+  }, function(err) {
+    console.log("Error on validationPromise: "+err);
+    
+  }); // end validationPromise
+} // end function
