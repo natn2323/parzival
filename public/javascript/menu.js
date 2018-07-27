8f8b5f4316620ae317310ec85ad0c2243518b9ac
Nathan Nguyen <natn2323@gmail.com>
2018-07-26 21:55:26 -0700

    Moved getSubPath to its own file. Made helpers and an additional route to get menu items.

--- public/javascript/menu.js
+++ public/javascript/menu.js
@@ -15,31 +15,30 @@ module.exports = {
 
 function GETHandler(request, response) {
   console.log("Menu gethandler entered");
-  var fs = require('fs');
-  fs.readFile('./public/html/menu.html', function(err, data) {
-    if(err) {
-      throw err;
-    } else {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-    }
-  });
 
+  let path_arr = require('./utils.js').getSubPath(request);
+  if (path_arr.length === 1 && path_arr[0] === "menu") {
+    var fs = require('fs');
+    fs.readFile('./public/html/menu.html', function(err, data) {
+      if(err) {
+        throw err;
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+      }
+    });
+  } else if (path_arr.length > 1
+    && path_arr[0] === "menu"
+    && path_arr[1] === "items") {
+      console.log("This is entered!");
+      getMenuItemsHandler(request, response);
+
+    } // end else if
 } // end GETHandler
 
 function POSTHandler(request, response, data) {
   console.log("Menu posthandler entered");
-
-  // An array representing the URL
-  var url_split_arr = require('url')
-    .parse(request.url, true)
-    .pathname
-    .split('/');
-  var url_split_arr_length = url_split_arr.length;
-  var url_split_path = url_split_arr.slice(1, url_split_arr_length);
-  var base = url_split_path[0];
-
   processOrderHandler(request, response, data);
 } // end POSTHandler
 
@@ -47,6 +46,49 @@ function POSTHandler(request, response, data) {
 /************************************************************************
  *************************** HELPER FUNCTIONS ***************************
  ************************************************************************/
+
+ function getMenuItemsHandler(request, response) {
+   let orderPromise = getMenuItems();
+   orderPromise.then(function(rows) {
+     if(rows) {
+       console.log("Menu items retrieved!");
+       // Parsing data
+       let dataToSubmit = {'content': []};
+       for(let i = 0; i < rows.length; i++) {
+         let unit = rows[i];
+         dataToSubmit['content'].push({
+           'itemName': unit.itemName,
+           'itemDescription': unit.itemDescription,
+           'unitPrice': unit.unitPrice
+         }); // end push
+       } // end for
+
+       response.writeHead('200', {'Content-Type': 'application/json'});
+       response.end(JSON.stringify(dataToSubmit));
+
+     } // end if
+   }) // end promise
+ } // end getMenuItemsHandler
+
+ function getMenuItems() {
+   return new Promise(function(resolve, reject) {
+     let db = require('./DBManager.js').getPool();
+
+     db.all('SELECT * FROM menuItems',
+       function(err, rows) {
+         if(err) {
+           reject(err);
+
+         } else if (rows.length>0) {
+           resolve(rows);
+
+         } else if (rows.length===0) {
+           resolve(false);
+
+         }
+     }); // end query
+   }); // end return
+ } // end getMenuItems
 
 function processOrderHandler(request, response, data) {
   var orderPromise = processOrder(data);
@@ -75,16 +117,16 @@ function processOrder(data) {
     var db = require('./DBManager.js').getPool();
 
     for(let i = 0; i < data['content'].length; i++) {
+      console.log("Process order is here!");
+      console.log("P.O. value is: "+data.content);
       let unit = data['content'][i];
 
       db.run("INSERT INTO orderedItems "
-        + "(itemName, quantity, unitPrice, username) VALUES"
-        + "($itemName, $quantity, $unitPrice, $username)",
+        + "(itemName, quantity) VALUES"
+        + "($itemName, $quantity)",
         {
           $itemName: unit.item,
-          $quantity: unit.quantity,
-          $unitPrice: unit.price,
-          $username: 'temporaryUser'
+          $quantity: unit.quantity
         },
         function(err) {
           if(!err) {
