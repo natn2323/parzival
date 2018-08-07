46f45d6cec541be58159feaf436463e0a68ee8f0
nguyenn2345 <Nathan.Nguyen2@panasonic.aero>
2018-08-07 10:37:41 -0700

    Removed the shotty authentication.

--- app.js
+++ app.js
@@ -18,37 +18,7 @@ require('./public/javascript/DBManager').getPool();
 */
 var serverPort = 8124;
 http.createServer(function (request, response) {
-
-  // authorization was found on StackOverflow
-  var auth = request.headers['authorization'];
-  if(!auth) {
-    response.statusCode = 401;
-    response.setHeader('WWW-Authenticate', 'Basic realm="Authorized User"');
-    response.end('<html><body>Authorization needed.</body></html>');
-
-  } else if(auth) {
-
-    var tmp = auth.split(' '); // auth format is: "Basic bmF0YW46TIz"
-    var buf = new Buffer(tmp[1], 'base64');
-    var plain_auth = buf.toString();
-
-    // console.log("Decoded authorization: "+plain_auth);
-
-    var creds = plain_auth.split(':');
-    var username = creds[0],
-        password = creds[1];
-
-    if(username == 'admin' && password == 'pass') {
-      //response.statusCode = 200;
-      dispatcher.serve(request, response); // This is the important part
-
-    } else {
-      response.statusCode = 401;
-      response.setHeader('WWW-Authenticate', 'Basic realm="Authorized User"');
-      response.end('<html><body>Wrong credentials.</body></html>');
-
-    }
-  }
+  dispatcher.serve(request, response);
 }).listen(serverPort);
 
 console.log('Server running at localhost: '+serverPort);
